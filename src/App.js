7d17d26dacb32972909cdb0a3fb9e4d4d0c6a200
Vadim <magnalov@mail.ru>
2021-02-27 13:02:47 +0300

    kek

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import "./App.scss";
 import React from "react";
+import Header from "./components/Header";
 
 function App() {
-  const [number, setNumber] = React.useState(0);
-  let plusNumber = () => setNumber(number + 1);
-  let resetNumber = () => setNumber(0);
-  
   return (
     <div>
-      <h1>{number}</h1>
-      <button onClick={plusNumber}> Нажми себе на жопу, долбоеб</button>
-      <div>
-        <button onClick={resetNumber}> Очисти</button>
+      <div className="wrapper">
+        <div className="wrapper--content">
+          <Header/>
+        </div>
       </div>
     </div>
   );
---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Header.scss";
+import icon from "../assets/header_icon.png";
+import amountIcon from "../assets/amount_icon.svg";
+
+const Header = ({ price, amount }) => {
+  return (
+    <div className="header">
+      <div className="header--logo">
+        <img src={icon} alt="" />
+        <div className="text">
+          <h1>React Pizza</h1>
+          <p>хотите питсы?</p>
+        </div>
+      </div>
+      <button className="header--button">
+              <div className="price">520{price} ₽</div>
+              <div className="line"></div>
+        <div className="amount">
+          <img src={amountIcon} alt="" />
+          <p>3</p>
+        </div>
+      </button>
+    </div>
+  );
+};
+
+export default Header;
